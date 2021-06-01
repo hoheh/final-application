e6335343710d2442a1019fc251136550940eb7df
Misha Skvor <hehhohhehhoh@gmail.com>
2021-06-01 20:25:13 +0300

    Добавил страницу регистрации пока что без функционала

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,14 +1,31 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 import WhiteBlock from "../components/WhiteBlock";
 
 import plus from "../assets/images/krest.svg";
 
+// TODO мб стоит объединить Auth и Register в один компонент [.]
+
+// TODO поменять цвет ринга у инпутов [.]
+
+// TODO перенести из старого проекта компоненты и стили [.]
+
+// TODO объединить компонент AUTH and REGISTER ->
+// TODO в один компонент и передавать информацию через пропсы + children [.]
+
+// TODO валидация гмыла и пароля с помощью регулярок [.]
+
+// TODO подключить firebase и добавить авторизацию через GOOGLE [.]
+
 const Auth = () => {
+  const history = useHistory();
+
   return (
     <WhiteBlock classname="flex flex-col">
-      <div className="flex flex-row-reverse cursor-pointer">
+      <div
+        onClick={() => history.goBack()}
+        className="flex flex-row-reverse cursor-pointer">
         <img className="w-7 h-7" src={plus} alt="" />
       </div>
       <div className="flex justify-center items-center mb-7">
@@ -21,7 +38,6 @@ const Auth = () => {
           </label>
           <input
             id="mail"
-            // TODO поменять цвет ринга
             placeholder="your_mail@example.com"
             className="w-full focus:ring border rounded-md font-thin border-gray-300 focus:outline-none px-2 py-2.5"
             type="text"
@@ -33,7 +49,6 @@ const Auth = () => {
           </label>
           <input
             id="pass"
-            // TODO поменять цвет ринга
             className="w-full focus:ring border rounded-md font-thin border-gray-300 focus:outline-none px-2 py-2.5"
             type="password"
           />
@@ -41,7 +56,7 @@ const Auth = () => {
       </div>
       <div>
         <div className="mt-8">
-          <button className="w-full rounded-md text-lg text-white py-3 focus:outline-none bg-green-500">
+          <button className="w-full rounded-md text-lg text-white py-3 focus:outline-none bg-green-500 focus:ring ring-green-500 focus:ring-offset-2">
             Войти в учетную запись
           </button>
         </div>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,12 +1,85 @@
 import React from "react";
+import { Link, useHistory } from "react-router-dom";
 
-import { WhiteBlock } from "../components/main";
+import WhiteBlock from "../components/WhiteBlock";
+
+import plus from "../assets/images/krest.svg";
 
 const Register = () => {
+  const history = useHistory();
+
   return (
-    <WhiteBlock>
+    <WhiteBlock classname="flex flex-col">
+      <div
+        onClick={() => history.goBack()}
+        className="flex flex-row-reverse cursor-pointer">
+        <img className="w-7 h-7" src={plus} alt="" />
+      </div>
+      <div className="flex justify-center items-center mb-7">
+        <p className="text-2xl font-medium">Регистрация</p>
+      </div>
+      <div className="space-y-6">
+        <div>
+          <label htmlFor="mail">
+            <span className="opacity-50 text-md">Электронная почта</span>
+          </label>
+          <input
+            id="mail"
+            // TODO поменять цвет ринга
+            placeholder="your_mail@example.com"
+            className="mt-0.5 w-full focus:ring border rounded-md font-thin border-gray-300 focus:outline-none px-2 py-2.5"
+            type="text"
+          />
+        </div>
+        <div>
+          <label htmlFor="name_surname">
+            <span className="opacity-50 text-md">ФИО</span>
+          </label>
+          <input
+            id="name_surname"
+            placeholder="Иванов Игорь Дмитриевич"
+            // TODO поменять цвет ринга
+            className="mt-0.5 w-full focus:ring border rounded-md font-thin border-gray-300 focus:outline-none px-2 py-2.5"
+            type="text"
+          />
+        </div>
+        <div>
+          <label htmlFor="pass">
+            <span className="opacity-50 text-md">Введите пароль</span>
+          </label>
+          <input
+            id="pass"
+            // TODO поменять цвет ринга
+            className="mt-0.5 w-full focus:ring border rounded-md font-thin border-gray-300 focus:outline-none px-2 py-2.5"
+            type="password"
+          />
+        </div>
+        <div>
+          <label htmlFor="repeat_pass">
+            <span className="opacity-50 text-md">Повторите пароль</span>
+          </label>
+          <input
+            id="repeat_pass"
+            // TODO поменять цвет ринга
+            className="mt-0.5 w-full focus:ring border rounded-md font-thin border-gray-300 focus:outline-none px-2 py-2.5"
+            type="password"
+          />
+        </div>
+      </div>
       <div>
-        <p>Зарегистрироваться</p>
+        <div className="mt-8">
+          <button className="w-full rounded-md text-lg text-white py-3 focus:outline-none bg-green-500 focus:ring ring-green-500 focus:ring-offset-2">
+            Зарегистрироваться
+          </button>
+        </div>
+        <div className="flex justify-center items-center mt-2">
+          <p className="text-s">
+            У меня уже есть аккаунт!
+            <Link to="/auth">
+              <span className="text-s ml-1 text-link">Войти</span>
+            </Link>
+          </p>
+        </div>
       </div>
     </WhiteBlock>
   );
